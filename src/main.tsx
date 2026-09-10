6d9b35127273879e76839df1b336a324112f917b
waxandwing <kellynyhagen@gmail.com>
2026-09-10 17:36:57 -0400

    Load Teaching Mode accessibility focus rules

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import { ErrorBoundary } from './app/ErrorBoundary';
 import './styles/tokens.css';
 import './styles/global.css';
+import './styles/teaching-accessibility.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
